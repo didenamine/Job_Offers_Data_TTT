8c83356a5c95f0a0d03065eb57f6aeead7024fbe
Diden Amine <d19amine@gmail.com>
2024-03-04 14:53:57 +0100

    test

--- test2.py
+++ test2.py
@@ -194,7 +194,7 @@ if response.status_code == 200:
                     JOBEXPERIENCE=None
                     JOBTECHSKILLS=None
                     JOBSOFTSKILLS =None
-                  
+                    'text'
                     for i,j in  zip(elements ,inside_elements): 
                         
                         if  'education' or 'educations' in i.lower() :
